63e066401b10a2cb7879f8b77f0ccc2418dd8d82
abfanck <arthur.fanck@gmail.com>
2018-12-01 15:12:24 -0200

    Update Door.java

--- Trabalho ES N/src/Equipment/Door.java
+++ Trabalho ES N/src/Equipment/Door.java
@@ -1,24 +1,55 @@
 package Equipment;
 
+import java.util.ArrayList;
+import java.time.LocalDate;
 import Data.*;
 import Interfaces.*;
 
 public class Door implements DoorInterface {
 
 	private int serialNum;
-
-	private String local;
-
+	private Position myPosition;
 	private boolean manualMode;
+	private List<Key> myKeys;
 
 	private IoTGatewayInterface ioTGatewayInterface;
 
-	public Position getPosition() {
-		return null;
+	public Door(int serialNum, Position position, boolean manualMode) {
+		this.serialNum = serialNum;
+		this.myPosition = position;
+		this.manualMode = manualMode;
+		this.myKeys = new ArrayList<>();
+	}
+	
+	public int getSerialNum() {
+		return this.serialNum;
+	}
+	
+	public Position getMyPosition() {
+		return this.myPosition;
+	}
+	
+	public List<Key> getMyKeys() {
+		return myKeys;
 	}
 
 	public boolean isKeyValid(Key key) {
-		return false;
+		Iterator iter = myKeys.iterator();
+		
+		while(iter.hasNext()) {
+			if(key.getID() == iter.next().getID()) {
+				if(key.getIsPermanent() == iter.next().getIsPermanent() && key.getIsPermanent() == true) {
+					return true;
+				} else if(key.getExpirationTime() == iter.next().getExpirationTime() && key.getExpirationTime() >= LocalDate.now()) {
+					return true;
+				} else {
+					myKeys.remove(key);
+					return false;
+				}
+			} else {
+				return false;
+			}
+		}
 	}
 
 }
