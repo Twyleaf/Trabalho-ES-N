9ce6c2ea2599e83544cf68f666a48f7278ced184
abfanck <arthur.fanck@gmail.com>
2018-12-01 12:24:52 -0200

    Update Key.java

--- Trabalho ES N/src/Data/Key.java
+++ Trabalho ES N/src/Data/Key.java
@@ -3,8 +3,26 @@ public class Key {
 
 	private int id;
 
-	private int expirationTime;
+	private Date expirationTime;
 
 	private boolean isPermanent;
+	
+	public Key(int keyID, Date keyExpirationTime, boolean keyIsPermanent) {
+		this.id = keyID;
+		this.expirationTime = keyExpirationTime;
+		this.isPermanent = keyIsPermanent;
+	}
+	
+	public int getID() {
+		return this.id;
+	}
+	
+	public Date getExpiraationTime() {
+		return this.expirationTime;
+	}
+	
+	public boolean getIsPermanent() {
+		return this.isPermanent;
+	}
 
 }
