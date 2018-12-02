1f2ece2fefba953a01a1a29eb986a30467e5072d
David Mees Knijnik <kdavidk98@gmail.com>
2018-12-02 16:02:22 -0200

    Mudanças David

--- Trabalho ES N/src/Interfaces/IoTGatewayInterface.java
+++ Trabalho ES N/src/Interfaces/IoTGatewayInterface.java
@@ -1,14 +1,20 @@
 
 package Interfaces;
 
+import java.util.List;
 import Equipment.IoTGateway;
+import java.time.LocalDateTime;
 
 public interface IoTGatewayInterface {
 
-	public abstract IoTGateway getInstance();
+	//public abstract IoTGateway getInstance();
 
 	public abstract void addCellphone(CellphoneInterface cellphone);
 
 	public abstract void addSmartwatch(int smartwatch);
+        
+        public List<Integer> getUserHourlySleepLevels();
+        
+        public void addCoffeeMachineTime(LocalDateTime time);
 
 }
